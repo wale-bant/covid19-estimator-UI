64b68e01007f587ee5a54c5f01a7a3f63db68838
wale-bant <wale.ashiru04@gmail.com>
2020-04-10 16:09:59 +0100

    overflow

--- src/component/Form.js
+++ src/component/Form.js
@@ -17,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(3),
   },
   avatar: {
     margin: theme.spacing(1),
